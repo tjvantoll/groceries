c6062e255b2501f0ccd467b6295ebaf467a42007
TJ VanToll <tj.vantoll@gmail.com>
2016-05-20 16:52:43 -0400

    Add the ability to add from history

--- src/app/pages/list/list.component.ts
+++ src/app/pages/list/list.component.ts
@@ -12,7 +12,9 @@ import {Config} from "../../shared/config";
 })
 export class ListComponent implements OnInit {
   groceryList: Array<Grocery>;
-  grocery: string;
+  history: Array<Grocery>;
+  grocery: string = "";
+  isShowingRecent = false;
 
   constructor(
     private _groceryListService: GroceryListService,
@@ -24,20 +26,42 @@ export class ListComponent implements OnInit {
       return;
     }
 
+    this.load();
+  }
+
+  load() {
+    this.groceryList = [];
+    this.history = [];
+
     this._groceryListService.load()
-      .subscribe(
-        groceryList => { this.groceryList = groceryList; }
-      );
+      .subscribe(loadedGroceries => {
+        loadedGroceries.forEach((groceryObject: Grocery) => {
+          if (groceryObject.deleted) {
+            this.history.unshift(groceryObject);
+          } else {
+            this.groceryList.unshift(groceryObject);
+          }
+        });
+      });
   }
   
   add() {
-    this._groceryListService.add(this.grocery)
-      .subscribe(groceryObject => {
-        this.groceryList.push(groceryObject);
+    if (this.grocery.trim() === "") {
+      alert("Enter a grocery item");
+      return;
+    }
 
-        // Clear out the input’s text
-        this.grocery = "";
-      });
+    this._groceryListService.add(this.grocery)
+      .subscribe(
+        groceryObject => {
+          this.groceryList.unshift(groceryObject);
+          this.grocery = "";
+        },
+        () => {
+          alert("An error occurred while adding an item to your list.");
+          this.grocery = "";
+        }
+      )
   }
 
   toggleDone(grocery: Grocery) {
@@ -48,12 +72,38 @@ export class ListComponent implements OnInit {
         alert("An error occurred managing your grocery list.");
       });
   }
-  
-  delete(grocery) {
-    this._groceryListService.deleteForever(grocery)
+
+  delete(grocery: Grocery) {
+    this._groceryListService.setDeleteFlag(grocery)
       .subscribe(() => {
         var index = this.groceryList.indexOf(grocery);
+        grocery.deleted = true;
         this.groceryList.splice(index, 1);
+        this.history.push(grocery);
+      });
+  }
+
+  toggleDoneHistory(grocery: Grocery) {
+    grocery.done = !grocery.done;
+  }
+
+  toggleRecent() {
+    let groceriesToRestore = []
+    this.history.forEach((grocery) => {
+      if (grocery.done) {
+        groceriesToRestore.push(grocery);
+      }
+    });
+
+    if (!this.isShowingRecent || groceriesToRestore.length == 0) {
+      this.isShowingRecent = !this.isShowingRecent;
+      return;
+    }
+
+    this._groceryListService.restore(groceriesToRestore)
+      .subscribe(() => {
+        this.isShowingRecent = false;
+        this.load();
       });
   }
 }
